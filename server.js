bbbc7358c32f8a20539e65c5d9dfb6c5ac5d03c9
Ravi Teja <iraviteja77@gmail.com>
2018-06-13 16:40:24 +0530

    Server Started

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`Server is running in port ${port}`);
+});
